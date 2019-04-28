4e21c5a2a1c820e9600ad358afff8b3f5e7b86eb
Ravi Veliyat <veliyat@gmail.com>
2019-04-28 20:38:01 +0530

    changed the port setting on the server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
